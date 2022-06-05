37d9f761bd244fffa000da68938026456dc3b10a
Priyanshu Shukla <priyanshus1@gmail.com>
2022-06-05 15:00:17 +0200

    init

---
+++ src/test/java/com/shukla/musify/MusifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shukla.musify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
